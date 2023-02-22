f1c9ce8f384c21f4e330b8612d72aad379fc1e63
Max-M-Dev <Max@example.com>
2023-02-22 17:02:23 +0200

    refactor: destructuring

--- src/components/formEntry/formEntryUi.tsx
+++ src/components/formEntry/formEntryUi.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import classes from "./formEntry.module.scss";
 import { PropsForUiType } from "./types";
 
-export function FormEntryUi({ children, title, action }: PropsForUiType) {
+export function FormEntryUi({ children, title, onSubmit }: PropsForUiType) {
 	return (
 		<div className="form-entry">
 			<div className="form-entry__wrapper">
 				<div className="form-entry__title">{title}</div>
-				<form action={action || "#"} className="form-entry__form">
+				<form className="form-entry__form" onSubmit={onSubmit}>
 					{children}
 				</form>
 			</div>
--- src/components/formEntry/types.ts
+++ src/components/formEntry/types.ts
@@ -4,5 +4,6 @@ export type PropsType = {
 	children: ReactNode;
 	title: string;
 	action?: string;
+	onSubmit: () => void;
 };
 export type PropsForUiType = PropsType & {};
